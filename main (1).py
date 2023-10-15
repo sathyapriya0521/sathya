1f84b5f91ddd0da958ce658fa47c85ee0a595461
sathyapriya0521 <147926782+sathyapriya0521@users.noreply.github.com>
2023-10-15 15:56:31 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,24 +1,48 @@
-# 1.1 Implement a recursive function to calculate the factorial of a given number.
-
-"""
-1!=1*1
-2!=2*1! --->2*1
-3!=3*2*1 --->3*1
-.
-.
-10!=10*9! --->10*9*8*...*1
-
-formula - n*(n-1)!
-"""
+'''implement a class called bank account that represents a bank account.The class should have private
+attributes for account number,account holdername, and account balance.Include methods to
+deposit money,withdraw money,and display the account balance.Ensure that the account balance
+cannot be accessed directly from outside the class.Write a program to create an instance of the 
+bank account class and the test the deposit and withdrawal functionality.'''
 
 
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
+class bankaccounts:
 
-number = int(input("enter a value:"))
-res=fact_rec(number)
+def__init__(self,account_number,account_holder_name,initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+        
+def deposit(self,amount):
+if amount>0:
+self.__account_balance+=amount
+#self.__account_balance=self.__account_balance+amount
+printf("Deposited ${}. New balance:${}".format(amount,
+self.__account_balance))
+else:
+printf("invalid depoist amount. please deposit a positive amount.")
 
-print("the factorial of {} is{}".format(number,res))
+def withdraw(self,amount):
+if amount>0and amount<=self.__account_balance:
+self.__account_balance-=amount
+#self.__account_balance=self.__account_balance-amount
+printf("withdraw ${}".format(amount,
+                                self.__account_balance))
+else:
+printf("invalid withdrawal amount or insufficient balance.")
+
+def display_balance(self):
+printf("account balance for {} (account #{}".format(
+    self.__account_holder_name,self.__account_number,
+    self.__account_balance))
+
+    
+# create an instance of the bank account class
+account=bank account(account_number="123456789",
+account_holder_name="hari prabu",
+initial_balance=5000.0)
+
+# test deposit and withdrawal functionality
+account.display_balance()
+account.deposit(500.0)
+account.withdrawal(200.0)
+account.display_balance()
--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-# leap year
+'''implement a class called player that represents a cricket player.The player should have a 
+method called play() which prints"The player is playing cricket".Derive two classes,batsman and
+bowler,from the player class.Override the play() method in each derived class to print "the batsman 
+is batting"and "The bowler is bowling",respectively.Write a program to create objects of both the
+batsman and bowler classes and call the play()method for each object.'''
 
-"""
-year % 4 ==0&
-year % 100 !=0/
-year %400==0
 
-"""
-def isLeapYear(year):
-  if (year % 4==0 and year % 100 !=0) or year % 400 ==0:
-    return True
-  else:
-    return False
-               
-year =int(input("enter a year:"))
-if isLeapYear(year):
-  print('{} is a leap year.'. format(year))
-else:
-  printf('{} is not a leap year.'. format(year))
+# define the base class player
+class player:
+  def play(self):
+    printf("the player is playing cricket.")
+    
+#define the derived class batsman
+class batsman(player):
+  def play(self):
+    printf("the batsman is battinng.")
+    
+#define the derived class bowler
+class bowler(player):
+def play(Self):
+  printf("the bowler is bowling.")
+
+#create objects of batsman and bowler class
+batsman=batsman()
+bowler=bowler()
+
+#call the play() method for each object
+batsman.play()
+bowler.play()
+
+
+
+
+
+  
